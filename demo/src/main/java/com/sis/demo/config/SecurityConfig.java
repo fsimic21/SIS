698f57c1939025118ed13fdbc3da5d01cb5ba653
Frano Šimić <fsimic21@student.foi.hr>
2024-10-18 22:13:46 +0200

    Napravljena je osnovna prijava i registracija sa emailom i lozinkom

---
+++ demo/src/main/java/com/sis/demo/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.sis.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(
+                                "/auth/register",
+                                "/auth/login")
+                        .permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
