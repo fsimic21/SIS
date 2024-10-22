895ba81ebd76bf1b232fab4aedbae0446a22e0b8
Mateo Vujica <mateo.vujica24@gmail.com>
2024-10-22 22:31:53 +0200

    Napravljena forma za prijavu koja šalje zahtjev na firebase, a ako je uspješna prijava potom će poslati zahtjev na backend.

---
+++ Vote.io/app/index.tsx
@@ -0,0 +1,65 @@
+import { Button, Text, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { signInWithEmailAndPassword} from 'firebase/auth'
+import { useState } from "react";
+import {auth} from '../firebaseConfig'
+import React from "react";
+
+export default function Index() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const login = async () => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+
+      const idToken = await user.getIdToken();
+
+      const response = await fetch('http://localhost:8080/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          token: idToken,
+        }),
+      });
+
+      const data = await response.json();
+      console.log('Backend response:', data);
+
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
+  };
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      {error ? <Text style={{ color: 'red', marginBottom: 10 }}>{error}</Text> : null}
+
+      <TextInput
+        style={{ borderWidth: 1, width: 200, marginBottom: 10, padding: 8 }}
+        placeholder="Email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+
+      <TextInput
+        style={{ borderWidth: 1, width: 200, marginBottom: 10, padding: 8 }}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+
+      <Button title="Login" onPress={login} />
+    </View>
+  );
+}
